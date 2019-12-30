bf2900ede7ad09a45120308b8e2ce6def3834ac6
amiao <2361222130@qq.com>
2019-12-30 15:17:21 +0800

    第二次提交

--- day48/src/com/winner/amiao/Amiao.java
+++ day48/src/com/winner/amiao/Amiao.java
@@ -3,7 +3,8 @@ package com.winner.amiao;
 public class Amiao {
 	public static void main(String[] args) {
 		//pwd查看当前路径
-		//git add表示吧这个文件添加到本地仓库里去
+		//git add文件名  表示把这个文件添加到git本地仓库里去
 		System.out.println("aaaaaa");
+		System.out.println("bbbbbb");
 	}
 }
